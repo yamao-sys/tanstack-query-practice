1d260ae4e9e821efeb8be43ac4eae64422097e22
yamao-sys <yamaopanku@gmail.com>
2025-03-02 17:30:26 +0900

    Prettierを追加

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // NOTE: Prettierとの競合を防ぐ設定を追加
+      //     : prettierConfigからルールを追加
+      ...eslintConfigPrettier.rules,
+    },
+  },
 ];
 
 export default eslintConfig;
