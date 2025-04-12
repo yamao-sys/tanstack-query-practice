3572afb8b1850f80be8cecbc19fc89b346fe8d38
yamao-sys <yamaopanku@gmail.com>
2025-04-12 16:48:54 +0900

    useMutationをTODO作成に適用

--- frontend/app/todos/new/_components/TodosNewForm/index.tsx
+++ frontend/app/todos/new/_components/TodosNewForm/index.tsx
@@ -5,6 +5,7 @@ import { FC, useState } from "react";
 import { useForm } from "react-hook-form";
 import { postTodos } from "../../_actions/todos";
 import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
 
 const INITIAL_VALIDATION_ERRORS = {
   title: [],
@@ -12,33 +13,37 @@ const INITIAL_VALIDATION_ERRORS = {
 };
 
 export const TodosNewForm: FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<StoreTodoInput>();
+  const { register, handleSubmit } = useForm<StoreTodoInput>();
 
   const [validationErrors, setValidationErrors] =
     useState<StoreTodoValidationError>(INITIAL_VALIDATION_ERRORS);
 
   const router = useRouter();
 
-  const onSubmit = handleSubmit(async (data) => {
-    setValidationErrors(INITIAL_VALIDATION_ERRORS);
+  const mutation = useMutation({
+    onMutate: () => setValidationErrors(INITIAL_VALIDATION_ERRORS),
+    mutationFn: async (data: StoreTodoInput): Promise<StoreTodoValidationError> =>
+      await postTodos(data),
+    onSuccess: (data, _variables, _context) => {
+      if (data === undefined) return;
 
-    const errors = await postTodos(data);
+      if (Object.keys(data).length > 0) {
+        setValidationErrors(data);
+        return;
+      }
 
-    if (Object.keys(errors).length > 0) {
-      setValidationErrors(errors);
-      return;
-    }
-
-    window.alert("TODOの作成に成功しました!");
-    router.push("/");
+      window.alert("TODOの作成に成功しました!");
+      router.push("/");
+    },
+    onError: (_error) => window.alert("予期しないエラーが発生しました."),
   });
+
+  const onSubmit = handleSubmit((data) => mutation.mutate(data));
 
   return (
     <>
+      {mutation.isPending && <p>Processing...</p>}
+
       <form onSubmit={onSubmit}>
         <div className='p-4 md:p-16'>
           <div className='w-4/5 md:w-3/5 mx-auto'>
@@ -54,9 +59,8 @@ export const TodosNewForm: FC = () => {
               <input
                 type='text'
                 className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                {...register("title", { required: true })}
+                {...register("title")}
               />
-              {errors.title && <span>タイトルは必須項目です。</span>}
               {validationErrors.title && (
                 <div className='w-full pt-5 text-left'>
                   {validationErrors.title.map((message, i) => (
@@ -81,9 +85,9 @@ export const TodosNewForm: FC = () => {
                 className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none'
                 {...register("content")}
               />
-              {validationErrors.title && (
+              {validationErrors.content && (
                 <div className='w-full pt-5 text-left'>
-                  {validationErrors.title.map((message, i) => (
+                  {validationErrors.content.map((message, i) => (
                     <p key={i} className='text-red-400'>
                       {message}
                     </p>
