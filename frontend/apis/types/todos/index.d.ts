9895e5fc9901b7a26975c2fee984f95f5431b955
yamao-sys <yamaopanku@gmail.com>
2025-04-13 19:36:03 +0900

    Todoの型を作る

--- frontend/apis/types/todos/index.d.ts
+++ frontend/apis/types/todos/index.d.ts
@@ -4,3 +4,6 @@ export type StoreTodoInput =
   components["requestBodies"]["StoreTodoInput"]["content"]["application/json"];
 
 export type StoreTodoValidationError = components["schemas"]["StoreTodoValidationError"];
+
+export type Todo =
+  components["responses"]["ShowTodoResponse"]["content"]["application/json"]["todo"];
